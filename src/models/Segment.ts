62c80823541073bc77274e4e759204fa69e3f0ab
miandrop <tattickattick@gmail.com>
2023-07-25 13:54:58 +0200

    Updates

--- src/models/Segment.ts
+++
@@ -1,9 +0,0 @@
-export class Segment {
-    word: string;
-    meaning: string | null;
-
-    constructor(word: string, meaning: string | null = null) {
-        this.word = word;
-        this.meaning = meaning;
-    }
-}
